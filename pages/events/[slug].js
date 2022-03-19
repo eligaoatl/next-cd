62feaf4929ed9258bb6e192976f1d56aa97a6c79
Eli Gao <eligaoatl@gmail.com>
2022-03-19 18:52:44 -0400

    init next.js tutorial

---
+++ pages/events/[slug].js
@@ -0,0 +1,5 @@
+export default function BuildEvent() {
+  return (
+    <div><h1>Build Event 1: starting build</h1></div>
+  )
+}
---
+++ pages/events/add.js
@@ -0,0 +1,5 @@
+export default function AddEvent() {
+  return (
+    <div><h1>Add Events</h1></div>
+  )
+}
